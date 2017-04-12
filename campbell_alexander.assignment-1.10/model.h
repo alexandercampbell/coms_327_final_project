76ebeb327ffeb5d8bd7a2c6e947f9158ffb6bdaa
Alexander Campbell <alexanderhcampbell@gmail.com>
2017-04-12 17:49:21 -0500

    model: define core gameplay models

--- campbell_alexander.assignment-1.10/model.h
+++ campbell_alexander.assignment-1.10/model.h
@@ -1,16 +1,67 @@
 
 #pragma once
 
+#include <stdbool.h>
+
 // no (non-stdlib) includes allowed :)
 
-#define DUNGEON_WIDTH  200
-#define DUNGEON_HEIGHT 100
+#define DUNGEON_WIDTH  120
+#define DUNGEON_HEIGHT 80
 #define DUNGEON_NUM_CELLS (DUNGEON_WIDTH * DUNGEON_HEIGHT)
+#define MAX_MOBS 50
 
 // Model definitions
 
+typedef struct dice {
+	int base;
+	int num;
+	int sides;
+} dice;
+
 typedef enum {
-	cell_grass = 0,
+	dagger,
+	sword,
+	axe,
+} weapon_type;
+
+typedef struct weapon {
+	char *name;
+	char *desc;
+	dice damage;
+} weapon;
+
+typedef struct ring {
+	char *name;
+	char *desc;
+
+	double damage_mul;
+	double hp_mul;
+} ring;
+
+typedef enum {
+	race_human,
+	race_elf,
+	race_orc,
+	race_dwarf,
+	race_reptile
+} race;
+
+typedef struct mob {
+	bool is_player;
+	bool is_friendly;
+
+	int hp;
+	int max_hp;
+
+	dice unarmed_attack;
+	weapon *weapon;
+	ring *ring;
+
+	race race;
+} mob;
+
+typedef enum {
+	cell_grass,
 	cell_tree,
 	cell_rock,
 	cell_tunnel,
@@ -18,6 +69,8 @@ typedef enum {
 
 typedef struct dungeon {
 	cell cells[DUNGEON_HEIGHT][DUNGEON_WIDTH];
+	int num_mobs;
+	mob mobs[MAX_MOBS];
 } dungeon;
 
 typedef enum key_press {
