7f0963e9e44cdc3ec36e395f1f3109776a6ab884
Alexander Campbell <alexanderhcampbell@gmail.com>
2017-04-16 17:03:13 -0500

    util: wrap macro args in parens
    Hopefully can avoid precedence issues this way.

--- campbell_alexander.assignment-1.10/util.hpp
+++ campbell_alexander.assignment-1.10/util.hpp
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 
-#define RAND_BETWEEN(a, b) (a + rand() % (b-a+1))
+#define RAND_BETWEEN(a, b) ((a) + rand() % ((b)-(a)+1))
 #define CLAMPED_TO(a, min, max) ((a) < (min) ? (min) : ((a) > (max)) ? (max) : (a))
 #define FRAND() ((float)rand()/(float)(RAND_MAX))
 
